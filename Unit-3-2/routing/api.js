e4e311dbb2df62146deeaff73c6c9c7ff75c88ff
harishtanniru <123283004+harishtanniru@users.noreply.github.com>
2023-06-25 15:11:49 -0500

    Unit-3 Submission

---
+++ Unit-3-2/routing/api.js
@@ -0,0 +1,134 @@
+var express = require("express");
+var router = express.Router();
+var baseController = require("../models/players");
+
+router.post("/add-player", async (req, res) => {
+  data = req.body;
+  baseController.create(data, (err, playerResponse) => {
+    if (err) {
+      return res.send({ response: err });
+    }
+    res.send({
+      response: playerResponse,
+    });
+  });
+});
+
+router.post("/update-player", (req, res) => {
+  let data = req.body;
+  baseController.findOneAndUpdate(
+    { _id: req.body._id },
+    data,
+    (err, updatedPlayer) => {
+      if (err) {
+        return res.send({ response: err });
+      }
+      res.send({
+        response: updatedPlayer,
+      });
+    }
+  );
+});
+
+router.post("/find-player", (req, res) => {
+  baseController.find({ _id: req.body._id }, (err, playerDetails) => {
+    if (err) {
+      return res.send({ response: err });
+    }
+    res.json({
+      response: playerDetails,
+    });
+  });
+});
+
+router.post("/delete-player", (req, res) => {
+  baseController.findOneAndRemove(
+    { _id: req.body._id },
+    (err, deletedPlayer) => {
+      if (err) {
+        return res.send({ response: err });
+      }
+
+      res.send({
+        response: deletedPlayer,
+      });
+    }
+  );
+});
+
+router.get("/find-all-players", (req, res) => {
+  baseController.find({}, (err, allPlayersDetails) => {
+    if (err) {
+      return res.send({ response: err });
+    }
+    res.json({
+      response: allPlayersDetails,
+    });
+  });
+});
+
+router.get("/get-all-rounder", (req, res) => {
+  baseController.find({ Role: "All rounder" }, (err, allPlayersDetails) => {
+    if (err) {
+      return res.send({ response: err });
+    }
+    res.json({
+      response: allPlayersDetails,
+    });
+  });
+});
+
+router.get("/get-batsmans", (req, res) => {
+  baseController.find({ Role: "Batsman" }, (err, allPlayersDetails) => {
+    if (err) {
+      return res.send({ response: err });
+    }
+    res.json({
+      response: allPlayersDetails,
+    });
+  });
+});
+
+router.get("/get-bowlers", (req, res) => {
+  baseController.find({ Role: "Bowler" }, (err, allPlayersDetails) => {
+    if (err) {
+      return res.send({ response: err });
+    }
+    res.json({
+      response: allPlayersDetails,
+    });
+  });
+});
+
+router.get("/get-players-by-wickets", (req, res) => {
+  baseController
+    .find({})
+    .sort({ Wickets: -1 })
+    .exec((err, allPlayersDetails) => {
+      if (err) {
+        return res.send({ response: err });
+      }
+
+      res.json({
+        response: allPlayersDetails,
+      });
+    });
+});
+
+router.get("/get-highest-hundreds", (req, res) => {
+  baseController
+    .find({})
+    .sort({ Hundreds: -1 })
+    .limit(1)
+    .exec((err, allPlayersDetails) => {
+      if (err) {
+        return res.send({ response: err });
+      }
+
+      res.json({
+        response: allPlayersDetails,
+      });
+    });
+});
+
+module.exports = router;
